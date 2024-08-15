888445fa5f562f6d5b367721da5677092687077e
Saurabh Raut <saurabhrmahadev1212@gmail.com>
2024-08-15 11:31:36 +0530

    worked on get started form using react-hook-form and zod for validation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,23 @@
 import type { Metadata } from 'next';
-import { Plus_Jakarta_Sans } from 'next/font/google';
 import './globals.css';
+import { Plus_Jakarta_Sans as FontSans } from 'next/font/google';
+import { ThemeProvider } from 'next-themes';
 
 import { cn } from '@/lib/utils';
-import { ThemeProvider } from '@/components/theme-provider';
-const fontSans = Plus_Jakarta_Sans({
+
+const fontSans = FontSans({
 	subsets: ['latin'],
 	weight: ['300', '400', '500', '600', '700'],
 	variable: '--font-sans',
 });
 
 export const metadata: Metadata = {
-	title: 'HealthCare management system',
-	description: 'Management App',
+	title: 'CarePulse',
+	description:
+		'A healthcare patient management System designed to streamline patient registration, appointment scheduling, and medical records management for healthcare providers.',
+	icons: {
+		icon: '/assets/icons/logo-icon.svg',
+	},
 };
 
 export default function RootLayout({
